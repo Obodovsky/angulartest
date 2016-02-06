f648f5f416089129c0a09fc43bf1c2a744e5d40f
VRyabinin <rcrrad@gmail.com>
2016-02-06 13:34:24 +0300

    Сделать центрирование

--- assets/js/directives/kitEditor.js
+++ assets/js/directives/kitEditor.js
@@ -1,15 +1,15 @@
-module.exports = ['d3Factory', function (d3Factory) {
+module.exports = ['d3Factory', '$window', '$q', function (d3Factory, $window, $q) {
   // DDO - Directive Definition Object
   return {
     scope: true,
     restrict: 'A',
-    link: function($scope, $element, $attrs) {
+    link: function ($scope, $element, $attrs) {
       // $scope унаследована от $rootScope
       // $element - обертка в jqLite
       // $attrs - массив атрибутов для DOM-элемента, на котором висит директива
-      d3Factory.d3().then(function(d3){
-
+      d3Factory.then(function (d3) {
         $scope.editor = {
+          testValue: 'c',
           behavior: {
             dragging: false
           },
@@ -66,9 +66,9 @@ module.exports = ['d3Factory', function (d3Factory) {
           .attr('width', 0)
           .attr('height', 0);
 
-        var DURATION = 800;
+        var DURATION = 4000;
 
-        var pageWidth = $scope.editor.pageProperties.widthMm *
+        var pageWidth  = $scope.editor.pageProperties.widthMm *
           $scope.editor.features.pixelsPerMm;
         var pageHeight = $scope.editor.pageProperties.heightMm *
           $scope.editor.features.pixelsPerMm;
@@ -86,13 +86,13 @@ module.exports = ['d3Factory', function (d3Factory) {
         linesX.enter().append('line')
           .attr('x1', 0)
           .attr('x2', 0)
-          .attr('y1', function(d){
+          .attr('y1', function (d) {
             return d;
           })
           .transition()
           .duration(DURATION)
           .attr('x2', pageWidth)
-          .attr('y2', function(d){
+          .attr('y2', function (d) {
             return d;
           });
 
@@ -103,39 +103,98 @@ module.exports = ['d3Factory', function (d3Factory) {
         linesY.enter().append('line')
           .attr('y1', 0)
           .attr('y2', 0)
-          .attr('x1', function(d){
+          .attr('x1', function (d) {
             return d;
           })
           .transition()
           .duration(DURATION)
           .attr('y2', pageHeight)
-          .attr('x2', function(d){
+          .attr('x2', function (d) {
             return d;
           });
 
         $scope.editor.behavior.d3 = {
           zoom: d3.behavior.zoom()
             .scale(1)
-          .scaleExtent([.2, 10])
-          .on('zoom', function(){
-            var t = d3.event.translate;
+            .scaleExtent([.2, 10])
+            .on('zoom', function () {
+              var t = d3.event.translate;
 
-            $scope.editor.svg.container
-              .attr('transform', 'translate(' + t + ')scale(' +
-                d3.event.scale + ')');
+              $scope.editor.svg.container
+                .attr('transform', 'translate(' + t + ')scale(' +
+                  d3.event.scale + ')');
 
-            t = t.toString().split(','); // "1, 2" -> [1, 2]
-            $scope.editor.position.x = t[0];
-            $scope.editor.position.x = t[1];
-          })
+              t = t.toString().split(','); // "1, 2" -> [1, 2]
+              $scope.editor.position.x = t[0];
+              $scope.editor.position.y = t[1];
+            })
         };
 
         g.call($scope.editor.behavior.d3.zoom);
         $scope.editor.behavior.d3.zoom.event(
           $scope.editor.svg.container);
 
+        /**
+         * Перемещение рабочей области в указанную точку.
+         * @param {Object} pt - Точка назначения
+         * @param {Number} pt.x - X-координата
+         * @param {Number} pt.y - Y-координата
+         * @returns {Promise}
+         */
+        $scope.translateTo = function (pt) {
+          // resolve - разрешить обещание
+          // reject - отклонить
+          return $q(function (resolve, reject) {
+            d3.transition('translateTo')
+              .duration(DURATION)
+              .tween('translateTo', function () {
+                // step - функция от t
+                // step(0)[0] -> $scope.editor.position.x
+                // step(0)[1] -> $scope.editor.position.y
+                // step(1)[0] -> pt.x
+                // step(1)[1] -> pt.y
+                var step = d3.interpolate(
+                  [$scope.editor.position.x, $scope.editor.position.y],
+                  [pt.x, pt.y]
+                );
+
+                function translateToInternal(x, y) {
+                  $scope.editor.behavior.d3.zoom.translate([x, y]);
+                  $scope.editor.behavior.d3.zoom.event(
+                    $scope.editor.svg.container);
+                  $scope.editor.position.x = x;
+                  $scope.editor.position.y = y;
+                }
+
+                // У возвращаемой функции должен быть один параметр - нормализованный интервал T
+                // t -> [0;1]
+                return function (t) {
+                  translateToInternal(step(t)[0], step(t)[1]);
+                }
+              }).each('end', function () {
+              resolve('Translated to (' + pt.x + ';' + pt.y + ') successfully');
+            });
+          });
+        };
+
+        $scope.center = function ($event) {
+          var scale        = $scope.editor.behavior.d3.zoom.scale();
+          var editorWidth  = $scope.editor.features.pixelsPerMm *
+            $scope.editor.pageProperties.widthMm * scale;
+          var editorHeight = $scope.editor.features.pixelsPerMm *
+            $scope.editor.pageProperties.heightMm * scale;
+          var center       = {
+            x: ($window.innerWidth - editorWidth) / 2,
+            y: ($window.innerHeight - editorHeight) / 2
+          };
+
+          return $scope.translateTo(center);
+        };
+
+        $scope.center().then(function (result) {
+          console.log(result);
+        });
       });
     }
   }
-
 }];
--- assets/js/modules/d3.js
+++ assets/js/modules/d3.js
@@ -10,28 +10,23 @@ angular.module('d3', []).factory('d3Factory', [
     // который станет доступен к какому-то моменту в будущем
 
     // defer - отложить [на будущее]
-    var d = $q.defer();
-
     var scriptTag = $document[0].createElement('script');
     scriptTag.async = true;
     scriptTag.type = 'text/javascript';
     scriptTag.src = '../../vendor/d3/d3.js';
+    var b = $document[0].body;
+    b.appendChild(scriptTag);
 
     // resolve - разрешить обещание
     // reject - отклонить
-    scriptTag.onload = function(){
-      $rootScope.$apply(function(){d.resolve($window.d3);})
-    };
-
-    var b = $document[0].body;
-
-    b.appendChild(scriptTag);
-
-    return {
-      d3: function(){
-        return d.promise;
-      }
-    }
-
+    return $q(function(resolve, reject) {
+      scriptTag.onload = function(){
+        $rootScope.$apply();
+        if ($window.d3)
+          resolve($window.d3);
+        else
+          reject($window.d3);
+      };
+    });
   }
 ]);
